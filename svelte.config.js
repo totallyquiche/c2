9e17a8aa8cf34b2af56c6647e2a9cc54a2477c2c
totallyquiche <bndady@gmail.com>
2025-06-21 01:57:45 -0500

    svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
     preprocess: vitePreprocess(),
-    kit: { adapter: adapter(), alias: { $components: 'src/components', $lib: 'src/lib' } }
+    kit: {
+        adapter: adapter(),
+        alias: { $components: 'src/components', $lib: 'src/lib', $types: 'src/types' }
+    }
 };
 
 export default config;
