635a87a4862a55f2dd4c4e160853c9452b5ae8dd
totallyquiche <bndady@gmail.com>
2025-06-20 22:47:24 -0500

    form component

---
+++ src/components/Form.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    let input = $state('');
+
+    function handleSubmit(event: Event) {
+        event.preventDefault();
+        input = input.trim();
+
+        if (input === '') return;
+    }
+</script>
+
+<!-- svelte-ignore a11y_autofocus -->
+<form class="flex flex-col gap-4">
+    <input
+        type="text"
+        placeholder="What?"
+        autofocus
+        class="border-light bg-light border-0.5 p-2"
+        bind:value={input}
+    />
+    <button
+        class="text-dark bg-accent focus:bg-accent cursor-pointer rounded-xl p-1 hover:brightness-95 active:scale-95"
+        onclick={handleSubmit}>Submit</button
+    >
+</form>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import Form from '$components/Form.svelte';
+</script>
+
+<div class="absolute flex h-full w-full items-center justify-center">
+    <Form />
+</div>
