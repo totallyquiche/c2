120faba3f87ff1473e41eb68a212a7fa0ad9eb89
totallyquiche <bndady@gmail.com>
2025-06-20 23:58:38 -0500

    add list server page

---
+++ src/components/List.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    let list: string[] = $state([]);
+
+    $effect(() => {
+        fetch('/list')
+            .then((response) => response.json())
+            .then((data) => {
+                list = data;
+            });
+    });
+</script>
+
+<ul class="flex flex-col gap-5">
+    {#each list as item}
+        <li>{item}</li>
+    {/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import Form from '$components/Form.svelte';
+    import List from '$components/List.svelte';
 </script>
 
-<div class="absolute flex h-full w-full items-center justify-center">
+<div class="flex flex-col items-center gap-8 overflow-y-auto p-4 pt-16">
     <Form />
+    <List />
 </div>
